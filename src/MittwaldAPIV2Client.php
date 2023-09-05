3e63cf494fbd62bf52682973fd1968af62139f68
Martin Helmich <m.helmich@mittwald.de>
2023-09-05 16:39:07 +0200

    Some documentation

--- src/MittwaldAPIV2Client.php
+++ src/MittwaldAPIV2Client.php
@@ -7,6 +7,18 @@ use Mittwald\ApiClient\Generated\V2\Clients\User\Authenticate\Authenticate200Res
 use Mittwald\ApiClient\Generated\V2\Clients\User\Authenticate\AuthenticateRequest;
 use Mittwald\ApiClient\Generated\V2\Clients\User\Authenticate\AuthenticateRequestBody;
 
+/**
+ * MittwaldAPIV2Client is the main entry point for the mStudio v2 API.
+ *
+ * It is a wrapper around the generated API client, and provides some more
+ * sophisticated authentication methods.
+ *
+ * Use the topic-specific factory methods to instantiate specific API clients:
+ *
+ *     $client->project()
+ *     $client->app()
+ *     $client->...
+ */
 class MittwaldAPIV2Client extends Client
 {
     protected final function __construct(string $baseUri, string|null $apiKey = null)
@@ -14,19 +26,47 @@ class MittwaldAPIV2Client extends Client
         parent::__construct($baseUri, $apiKey);
     }
 
+    /**
+     * Instantiates a new client with an existing API token.
+     *
+     * This is the recommended way of instantiating a new API client. You can
+     * create a new API token in the mStudio UI, or via the API itself
+     *
+     * @param string $apiToken The API token
+     * @return static
+     */
     public static function newWithToken(string $apiToken): static
     {
         return new static('https://api.mittwald.de/v2/', $apiToken);
     }
 
+    /**
+     * Instantiates a new client without an API token.
+     *
+     * This is useful for endpoints that do not require authentication, such as
+     * the user registration endpoint, or the authentication endpoints themselves.
+     *
+     * @return static
+     */
     public static function newUnauthenticated(): static
     {
         return new static('https://api.mittwald.de/v2/');
     }
 
+    /**
+     * Instantiates a new client with an email and password.
+     *
+     * This is NOT recommended, as it requires the client to store the password
+     * in plain text. Also, this authentication method will not work if the user
+     * has 2FA enabled.
+     *
+     * @param string $email The email address of your mStudio user.
+     * @param string $password The password of your mStudio user.
+     * @return static
+     */
     public static function newWithCredentials(string $email, string $password): static
     {
-        $authenticateRequest = new AuthenticateRequest(new AuthenticateRequestBody($email, $password));
+        $authenticateRequest  = new AuthenticateRequest(new AuthenticateRequestBody($email, $password));
         $authenticateResponse = static::newUnauthenticated()->user()->authenticate($authenticateRequest);
 
         return match (get_class($authenticateResponse)) {
