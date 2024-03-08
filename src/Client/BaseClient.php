ed46210e214759ef4310cc10b58064c1d3b4959f
Martin Helmich <m.helmich@mittwald.de>
2024-03-08 18:20:01 +0100

    Add exponential backoff

--- src/Client/BaseClient.php
+++ src/Client/BaseClient.php
@@ -16,9 +16,12 @@ abstract class BaseClient
         $stack->push(GuzzleRetryMiddleware::factory([
             // 403 needs to be included here, as the API's ACL layer is eventually consistent
             // and might return 403 for a short period of time after a resource has been created
-            'retry_on_status'    => [403, 429, 503, 500],
-            'max_retry_attempts' => 10,
-            'retry_on_methods'   => ['GET', 'HEAD'],
+            'retry_on_status'          => [403, 429, 503, 500],
+            'max_retry_attempts'       => 10,
+            'retry_on_methods'         => ['GET', 'HEAD'],
+
+            // Exponential backoff, base 2 starting at 100ms, capped at 4 seconds
+            'default_retry_multiplier' => fn(int $retries): float => min(4, 2 ** ($retries - 1) * .1),
         ]));
 
         $opts = [
